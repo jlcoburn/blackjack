9c2b4d3041b37ead9a9d099f626029e71672373e
Jason Coburn <jlewiscoburn@gmail.com>
2017-01-17 05:28:02 -0500

    Add code to determine value of face cards

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 function Player() {
   this.hand = [];
   this.total = 0;
+  this.value = 0;
 
   this.draw = function(num) {
     for(let i = 0; i < num; i++) {
@@ -13,14 +14,18 @@ function Player() {
 
   this.addTotal = function() {
     for(let i = 0; i < this.hand.length; i++) {
-      this.total+=parseInt(this.hand[i][2]);
+      if (this.hand[i][2] == 'K' || this.hand[i][2] == 'Q' || this.hand[i][2] == 'J') {
+            this.value = 10;
+      } else {
+        this.value = parseInt(this.hand[i][2]);
+      }
+      this.total+=this.value;
     }
   };
 
   this.showHand = function() {
     document.getElementById("pCards").innerHTML = this.hand;
-  }
-
+  };
 }
 
 var p = new Player();
